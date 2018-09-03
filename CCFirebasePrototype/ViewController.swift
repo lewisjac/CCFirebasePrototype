76d3364a3355722129864d08fe0903f53b2bdd65
Jack Simmons <jacksimlewis@gmail.com>
2018-09-03 10:17:21 -0700

    Initial Commit

---
+++ CCFirebasePrototype/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CCFirebasePrototype
+//
+//  Created by Jack Simmons on 9/3/18.
+//  Copyright © 2018 Jack Simmons. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
