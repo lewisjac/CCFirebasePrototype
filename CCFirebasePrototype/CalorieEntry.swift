9e28bfad89d98a9083374202744215bd3bc74c5a
Jack Simmons <jacksimlewis@gmail.com>
2018-09-12 07:14:17 -0700

    first commit

---
+++ CCFirebasePrototype/CalorieEntry.swift
@@ -0,0 +1,9 @@
+//
+//  CalorieEntry.swift
+//  CCFirebasePrototype
+//
+//  Created by Jack Simmons on 9/3/18.
+//  Copyright © 2018 Jack Simmons. All rights reserved.
+//
+
+import Foundation
---
+++ CCFirebasePrototype/ExtractData.swift
@@ -0,0 +1,9 @@
+//
+//  ExtractData.swift
+//  CCFirebasePrototype
+//
+//  Created by Jack Simmons on 9/4/18.
+//  Copyright © 2018 Jack Simmons. All rights reserved.
+//
+
+import Foundation
